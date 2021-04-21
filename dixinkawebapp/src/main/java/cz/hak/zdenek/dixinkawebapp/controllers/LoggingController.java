ad7fdc37225c36c34a58c6041885ac79e8e50df5
Zdenek Hak <zdendahak@gmail.com>
2021-04-21 10:45:27 +0200

    logback closes #3

---
+++ dixinkawebapp/src/main/java/cz/hak/zdenek/dixinkawebapp/controllers/LoggingController.java
@@ -0,0 +1,21 @@
+package cz.hak.zdenek.dixinkawebapp.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class LoggingController {
+
+    @RequestMapping("/")
+    public String index() {
+        log.trace("A TRACE Message");
+        log.debug("A DEBUG Message");
+        log.info("An INFO Message");
+        log.warn("A WARN Message");
+        log.error("An ERROR Message");
+
+        return "Howdy! Check out the Logs to see the output...";
+    }
+}
